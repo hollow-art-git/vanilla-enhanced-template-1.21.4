2266d8154da723f2e56da26ed58fa432e073c4ca
hollow <samuel.garciapueblas@gmail.com>
2025-02-19 20:56:06 +0100

    Initial commit

---
+++ src/main/java/hollow/vanillaenhanced/VanillaEnhancedClient.java
@@ -0,0 +1,10 @@
+package hollow.vanillaenhanced;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VanillaEnhancedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
