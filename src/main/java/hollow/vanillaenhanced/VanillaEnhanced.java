f4c2083d2d18d36f048c4848496f2a1ec83770a9
hollow <samuel.garciapueblas@gmail.com>
2025-02-19 20:58:06 +0100

    test

--- src/main/java/hollow/vanillaenhanced/VanillaEnhanced.java
+++ src/main/java/hollow/vanillaenhanced/VanillaEnhanced.java
@@ -5,6 +5,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//Commit test
+
 public class VanillaEnhanced implements ModInitializer {
 	public static final String MOD_ID = "vanilla-enhanced";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
