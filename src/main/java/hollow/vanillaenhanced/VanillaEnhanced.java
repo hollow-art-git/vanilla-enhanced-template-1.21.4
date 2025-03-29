eb99fd30d5b59f1b1d1b8d2faf2ca98752f239ea
hollow <samuel.garciapueblas@gmail.com>
2025-03-29 01:42:10 +0100

    wells, stones and tents

--- src/main/java/hollow/vanillaenhanced/VanillaEnhanced.java
+++ src/main/java/hollow/vanillaenhanced/VanillaEnhanced.java
@@ -4,8 +4,16 @@ import hollow.vanillaenhanced.block.ModBlocks;
 import hollow.vanillaenhanced.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.village.TradeOffer;
+import net.minecraft.village.TradedItem;
+import net.minecraft.village.VillagerProfession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
 
 //Commit test
 //https://docs.fabricmc.net/develop/getting-started/project-structure#entrypoints
@@ -19,5 +27,9 @@ public class VanillaEnhanced implements ModInitializer {
 	public void onInitialize() {
 		ModItems.initialize();
 		ModBlocks.initialize();
+
+		// VILLAGER TRADES
+
+		
 	}
 }
